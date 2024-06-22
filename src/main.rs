365aeb52e4547c24c5cccaf4993b79ceb5582dd9
Khanh Duy <duykhanh471@protonmail.com>
2024-06-22 12:50:15 +0700

    first update
    nothing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::OpenOptions;
 use std::io::{Write, stdout};
 use std::{thread, time};
 use std::sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}};
@@ -11,7 +12,8 @@ struct Cli {}
 
 fn main() {
     let _cli = Cli::parse();
-
+    // 
+    let mut status_file = OpenOptions::new().append(true).create(true).write(true).open("timeline.txt").expect("Can't open the timeline.txt file");
     // Async object to control run of programme
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
@@ -25,13 +27,13 @@ fn main() {
     // count up in 10ths of a second for precision.
     let mut partial_seconds_counter: usize = 0;
     let partials_in_second = 10;
-
+    
+    let mut total_time: usize = 0;
     // Handle Ctrl + C:
     // https://rust-cli.github.io/book/in-depth/signals.html
     ctrlc::set_handler(move || {
         // Tell programme to exit
         r.store(false, Ordering::SeqCst);
-
         // Show cursor before exitting
         let mut stdout = s.lock().unwrap();
         stdout.execute(cursor::Show).unwrap();
@@ -52,7 +54,6 @@ fn main() {
         if (partial_seconds_counter % partials_in_second) == 0 {
             // Calculate total seconds expended by the programme
             let seconds = partial_seconds_counter / partials_in_second;
-
             // Write to time stdout:
             // https://stackoverflow.com/a/59890400/12069968
             stdout.queue(cursor::SavePosition).unwrap();
@@ -62,12 +63,13 @@ fn main() {
             stdout.queue(cursor::RestorePosition).unwrap();
             stdout.queue(terminal::Clear(terminal::ClearType::FromCursorDown)).unwrap();
         }
-
         // Sleep for one-tenth of a seecond
         thread::sleep(time::Duration::from_millis(100));
+        total_time += 1;
         partial_seconds_counter += 1;
     }
-
+    let (x, y, z) = tuple_converter(total_time / 10);
+    writeln!(status_file, "Immersion: {}:{}:{}", x, y, z).expect("Can't write to the file");
     // Ensure cursor is shown before exitting
     stdout.execute(cursor::Show).unwrap();
 }
@@ -83,4 +85,11 @@ fn format_seconds(seconds: usize) -> String {
         minutes_rem,
         seconds_rem
     )
+}
+
+fn tuple_converter(seconds: usize) -> (usize, usize, usize) {
+    let seconds_rem = seconds % 60;
+    let minutes_rem = (seconds / 60) % 60;
+    let hours_rem = (seconds / 60) / 60;
+    (hours_rem, minutes_rem, seconds_rem)
 }
